ae0194309065e63a3505c48bb6abdd36c5752ef4
Borja Aday Guadalupe Luis <borjagua@ucm.es>
2021-03-12 16:03:21 +0000

    Corregido problema - Subsecuencia común más larga

--- Subsecuencia común más larga/main.cpp
+++ Subsecuencia común más larga/main.cpp
@@ -9,39 +9,30 @@ using namespace std;
 
 // Programacion dinamica descendente. El coste está en O(N*M) siendo N el tamano de la cadena1 y M el tamano de la cadena2.
 // Esto se debe a que vamos descartando caracteres de ambas cadenas hasta agotar una o ambas.
-int maxSubsecuencia(string const& cadena1, string const& cadena2, int i, int j, Matriz<int>& tabla) {
-    if (i < 0 || j < 0) {
-        return 0;
-    }
-    int& res = tabla[i][j];
-    if (res == -1) {
-        if (i >= 0 && j >= 0 && cadena1[i] == cadena2[j]) {
-            res = maxSubsecuencia(cadena1, cadena2, i - 1, j - 1, tabla) + 1;
-        } 
-        else if (i >= 0 && j >= 0 && cadena1[i] != cadena2[j]) {
-            res = max(maxSubsecuencia(cadena1, cadena2, i - 1, j, tabla), maxSubsecuencia(cadena1, cadena2, i, j - 1, tabla));
+void maxSubsecuencia(string const& cadena1, string const& cadena2, int const& n1, int const& n2, Matriz<int>& tabla) {
+    for (int i = 0; i < n1 + 1; i++) tabla[i][0] = 0;
+    for (int j = 0; j < n2 + 1; j++) tabla[0][j] = 0;
+
+    for (int i = 1; i < n1 + 1; i++) {
+        for (int j = 1; j < n2 + 1; j++) {
+            if (cadena1[i - 1] == cadena2[j - 1]) tabla[i][j] = tabla[i - 1][j - 1] + 1;
+            else tabla[i][j] = max(tabla[i - 1][j], tabla[i][j - 1]);
         }
     }
-    return res;
 }
 
-// El coste de esta función en el caso peor esta en O(N*M) siendo N el tamano de la cadena1 y M el tamano de la cadena2.
-void reconstruir(string const& cadena1, string const& cadena2, Matriz<int> const& tabla, int i, int n2, int tam, string & sol) {
-    int j = 0;
-    if (tam > 0)
-    {
-        bool encontrado = false;
-        while (j < n2 && !encontrado)
-        {
-            if (tabla[i][j] == tam)
-                encontrado = true;
-            else
-                j++;
+void reconstruir(string const& cadena1, string const& cadena2, int const& n1, int const& n2, Matriz<int> const & tabla, string & sol) {
+    int i = n1, j = n2, maxSubsecuencia = tabla[i][j];
+    while (maxSubsecuencia > 0) {
+        if (cadena1[i - 1] == cadena2[j - 1]) {
+            sol.push_back(cadena1[i - 1]);
+            i--; j--;
+            maxSubsecuencia--;
         }
-        if(j == n2) reconstruir(cadena1, cadena2, tabla, i - 1, n2, tam, sol);
         else {
-            reconstruir(cadena1, cadena2, tabla, i - 1, j, tam - 1, sol);
-            sol.push_back(cadena2[j]);
+            if (tabla[i - 1][j] == tabla[i][j - 1]) i--;
+            else if (tabla[i - 1][j] != tabla[i][j - 1] && tabla[i][j] == tabla[i - 1][j]) i--;
+            else j--;
         }
     }
 }
@@ -57,15 +48,18 @@ bool resuelveCaso() {
     cin >> cadena2;
 
     int n1 = cadena1.size(), n2 = cadena2.size();
-    Matriz<int> tabla(n1, n2, -1);
+    Matriz<int> tabla(n1 + 1, n2 + 1, -1);
 
-    int max = maxSubsecuencia(cadena1, cadena2, n1 - 1, n2 - 1, tabla);
+    maxSubsecuencia(cadena1, cadena2, n1, n2, tabla);
 
-    string sol = "";
-    
-    reconstruir(cadena1, cadena2, tabla, n1 - 1, n2, max, sol);
-
-    cout << sol << '\n';
+    if (tabla[n1][n2] > 0) {
+        string sol = "";
+        reconstruir(cadena1, cadena2, n1, n2, tabla, sol);
+        for (int i = sol.size() - 1; i >= 0; i--) {
+            cout << sol[i];
+        }
+    }
+    cout << '\n';
 
     return true;
 }
